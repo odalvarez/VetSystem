03da6fb09f275735cbea6245c599af423166cda6
Oscar Alvarez <Duvan2604@gmail.com>
2026-06-13 00:13:29 -0500

    add migration for nullable owner email in reminder jobs

--- services/notifications-service/NotificationsService.Infrastructure/Migrations/20260613051259_make_owner_email_nullable.Designer.cs
+++ services/notifications-service/NotificationsService.Infrastructure/Migrations/20260613051259_make_owner_email_nullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NotificationsService.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using NotificationsService.Infrastructure.Data;
 namespace NotificationsService.Infrastructure.Migrations
 {
     [DbContext(typeof(NotificationsDbContext))]
-    partial class NotificationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260613051259_make_owner_email_nullable")]
+    partial class make_owner_email_nullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
